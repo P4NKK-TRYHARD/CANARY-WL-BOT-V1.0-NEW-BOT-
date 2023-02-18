0836684d4f90423393c67dd9f7c0d847f87011f8
P4NKK_TRYHARD <91852700+P4NKK-TRYHARD@users.noreply.github.com>
2023-02-18 13:11:40 +0100

    Update wl.js

--- comandos/wl.js
+++ comandos/wl.js
@@ -1,27 +1,38 @@
 const Discord = require('discord.js');
 
+
+
 module.exports = {
   name: "aprobado",
   aliases: ["aprobado"],
   execute: async(client, message, args) => {
 
+  
+  if(!message.member.roles.cache.has('1075808799878823976')) { return message.channel.send("``Error ❌`` ¡Usted no tiene los permisos suficientes!"); }
 const mencionado = message.mentions.users.first();
 if(!mencionado) return message.reply('``❌ Error`` No ha mencionando a ningún usuario.');
+let role = message.guild.roles.cache.get("1075808829612245144");
+let member = message.mentions.members.first();
 let id = mencionado.id;
+
+
 
 const embedDatos = new Discord.MessageEmbed() 
         
-   .setTitle("<a:aprobado:1041814002239537213> __**Whitelist Aprobada**__ <a:aprobado:1041814002239537213>")
+   .setTitle("✅ __**Whitelist Aprobada**__ ✅")
    .setColor("#12F708")
    .setDescription(`¡Felicidades! ${mencionado}
    Has aprobado las pruebas, pasa a probar y jugar el servidor.
    ¡Te esperamos dentro!`)
-   .setFooter("© CanaryTM ", client.user.avatarURL())
+   .setFooter(`© WonderRP corregida por `+ message.author.username , client.user.avatarURL())
 
-client.channels.cache.get("1041810520963039282").send({ embeds: [embedDatos] }) 
-message.channel.send("Wl enviada correctamente")
-      
-      
+client.channels.cache.get("1075808886965145722").send({ embeds: [embedDatos] })
+member.roles.add(role).catch(console.error);
+const user = message.author;
+return message.channel.send(`<@${user.id}> Wl enviada correctamente`);
+
+
+
 message.delete(5000)
   }
 }
